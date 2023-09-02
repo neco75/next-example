a43dd55e4fcbec16d46395ad94ad801136029744
sagara_rikuto <sagara_rikuto@plise.co.jp>
2023-09-03 00:44:33 +0900

    study-components

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <footer>
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <img src="/vercel.svg" alt="Vercel Logo" />
+      </a>
+    </footer>
+  );
+}
